8faddb380684eada51a02755bafd67f2482ac849
goibnda <gobindabauri008@gmail.com>
2023-12-15 21:31:18 +0530

    add all file

---
+++ src/main/java/com/infotrixs/Social/Media/Dashboard/Backned/SocialMediaDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.infotrixs.Social.Media.Dashboard.Backned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotrixs/Social/Media/Dashboard/Backned/SocialMediaDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotrixs.Social.Media.Dashboard.Backned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
