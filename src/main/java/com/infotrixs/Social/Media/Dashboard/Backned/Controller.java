22b2b9a915d63b822ce07b977143d992caf6e84b
goibnda <gobindabauri008@gmail.com>
2023-12-19 00:40:53 +0530

    add cross origin

--- src/main/java/com/infotrixs/Social/Media/Dashboard/Backned/Controller.java
+++ src/main/java/com/infotrixs/Social/Media/Dashboard/Backned/Controller.java
@@ -2,10 +2,12 @@ package com.infotrixs.Social.Media.Dashboard.Backned;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5500")
 @RestController
 public class Controller {
     @Autowired
